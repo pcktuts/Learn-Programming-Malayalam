ae1b88ef3a9529e69b793cc9700acaa67986fb91
Learn Programming <pcktuts@gmail.com>
2021-08-24 14:12:23 +0530

    changed the default login redirect url, added identity scaffold pages for login and register for future customization.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Learn_Programming_Malayalam.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Learn_Programming_Malayalam.Areas.Identity.IdentityHostingStartup))]
+namespace Learn_Programming_Malayalam.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
